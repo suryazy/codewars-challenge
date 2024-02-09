5f72cfabc766c950d17166b73f9124ac0d2326f2
Surya <surya.zfy@gmail.com>
2024-02-09 19:31:36 +0800

    tambah soal 10-11

---
+++ 11-Descending Order.js
@@ -0,0 +1,14 @@
+function descendingOrder(n) {
+  const arr = Array.from(String(n), Number);
+  arr.sort(function (a, b) {
+    return b - a;
+  });
+  return arr.reduce((accum, digit) => accum * 10 + digit, 0);
+}
+
+// function descendingOrder(n) {
+//   return parseInt(String(n).split("").sort().reverse().join(""));
+// }
+
+console.log(descendingOrder(12345));
+console.log(descendingOrder(572481));
